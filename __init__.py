af1f93231d9869c05d2c08638c05267406bab6ae
Andrew Peel <34818078+CreativeDesigner3D@users.noreply.github.com>
2026-08-20 15:06:37 -0700

    Update Version Number

--- __init__.py
+++ __init__.py
@@ -36,7 +36,7 @@ from bpy.app.handlers import persistent
 bl_info = {
     "name": "Home Builder 5",
     "author": "Andrew Peel",
-    "version": (5, 1, 55),
+    "version": (5, 1, 56),
     "blender": (5, 0, 0),
     "location": "3D Viewport Sidebar",
     "description": "Library for Designing Interior Spaces",
